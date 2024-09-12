12f97b6402f940bf3dcbf635ecf6b19b777b6b85
Mark A. Hix <markahix@gmail.com>
2024-09-12 15:10:34 -0400

    cleaning of gitignore

--- obj/cpptraj.d
+++
@@ -1,8 +0,0 @@
-obj/cpptraj.o: src/cpptraj.cpp include/cpptraj.h include/utilities.h \
- include/classes.h
-
-include/cpptraj.h:
-
-include/utilities.h:
-
-include/classes.h:
--- obj/python.d
+++
@@ -1,8 +0,0 @@
-obj/python.o: src/python.cpp include/python.h include/utilities.h \
- include/classes.h
-
-include/python.h:
-
-include/utilities.h:
-
-include/classes.h:
